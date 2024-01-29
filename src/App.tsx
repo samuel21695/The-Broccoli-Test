6bdfc09526f8452ce287de8b0e89fa321879e75b
samuel21695 <samuel21695@gmail.com>
2024-01-29 22:04:35 +0900

    ✨ import footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Main from './components/main';
+import Footer from './layouts/Footer';
 
 function App() {
   return (
     <div className="App">
       <Main></Main>
+      <Footer></Footer>
     </div>
   );
 }
