0cb0b52f1c807685a93e4c52a183e812a17f6f21
samuel21695 <samuel21695@gmail.com>
2024-01-29 22:39:47 +0900

    🎨 broccoli

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,8 +3,14 @@ import './Main.css';
 function Main() {
   return (
     <div id='main'>
-      <div className='css-1c'></div>
-      <div className='css-2c'></div>
+      <div className='css-1c'>
+        <h1>Am I really hungry?</h1>
+        <p>Feel free to test that you are really hungry or just bored with the broccoli test.</p>
+        <p>Ask yourself “would I eat raw broccoli right now?”</p>
+      </div>
+      <div className='css-2c'>
+        <div className='broccoli'></div>
+      </div>
     </div>
   );
 }
