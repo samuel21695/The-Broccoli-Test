7d8032442724da8309170a310c1bafc10c16d1e3
samuel21695 <samuel21695@gmail.com>
2024-01-31 15:24:28 +0900

    ✨ Router empty structure

--- src/layouts/Router.tsx
+++ src/layouts/Router.tsx
@@ -3,3 +3,10 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Main from '../pages/Main';
 import Test from '../pages/test/Test';
 
+function Router () {
+  return(
+    <></>
+  )
+}
+
+export default Router;
