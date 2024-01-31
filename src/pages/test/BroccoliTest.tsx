ff03882374c3a5edc981c2a1e370546d7d4a5ac2
samuel21695 <samuel21695@gmail.com>
2024-01-31 18:02:47 +0900

    ✨ onAnimationEnd function

--- src/pages/test/BroccoliTest.tsx
+++ src/pages/test/BroccoliTest.tsx
@@ -12,7 +12,15 @@ function Test () {
     if( animationCount < 5) {
       setAnimationCount(animationCount + 1);  // 애니메이션 횟 수 증가
     }
-}
+  }
+
+  const onAnimationEnd = () => {
+    // 애니메이션이 5번 실행된 후 브쿠롤리 수를 증가시키고 애니메이션 횟수 초기화
+    if (animationCount === 5) {
+      setEatenBroccoli(eatenBroccoli + 1); // 먹은 브로콜리 수 증가
+      setAnimationCount(0); // 애니메이션 횟수 초기화
+    } 
+  }
 
   return (
     <div className='test-container'>
