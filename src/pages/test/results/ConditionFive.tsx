7986145bda4236982880e02e5b3ae6615928f493
samuel21695 <samuel21695@gmail.com>
2024-02-01 13:11:27 +0900

    🎨 style 7 layout change

--- src/pages/test/results/ConditionFive.tsx
+++ src/pages/test/results/ConditionFive.tsx
@@ -1,6 +1,9 @@
 function ConditionFive() {
   return (
-    <></>
+    <>
+      <h1>You are now <span style={{ color: "#F00101"}}>"Condition"</span></h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore natus soluta corrupti. Molestiae, possimus deserunt. Temporibus quod mollitia omnis quaerat ipsum a amet, fugiat et possimus illum? Assumenda, illo voluptas!</p>
+    </>
   );
 }
 
