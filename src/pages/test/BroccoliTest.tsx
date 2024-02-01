15d010ac65520fe8236cba797d8f5b6fa20125f6
samuel21695 <samuel21695@gmail.com>
2024-02-01 10:47:47 +0900

    🎨 biteAnimation protoype

--- src/pages/test/BroccoliTest.tsx
+++ src/pages/test/BroccoliTest.tsx
@@ -4,23 +4,20 @@ import './BroccoliTest.css'
 function Test () {
   // 먹은 브로콜리 수와 애니메이션 횟수를 상태로 관리
   const [eatenBroccoli, setEatenBroccoli] = useState(0);
-  const [animationCount, setAnimationCount] = useState(0);
   
   // 브로콜리를 클릭했을 때 실행되는 함수
   const eatBroccoli = () => {
     // 애니메이션 횟수가 5 미만일 때 애니메이션 실행
-    if( animationCount < 5) {
-      setAnimationCount(animationCount + 1);  // 애니메이션 횟 수 증가
+    if( eatenBroccoli < 5) {
       setEatenBroccoli(eatenBroccoli + 1); // 먹은 브로콜리 수 증가
     }
   }
 
-
   return (
     <div className='test-container'>
-      {animationCount < 5 && (
+      {eatenBroccoli < 5 && (
         <div 
-          className={`target animation-${animationCount + 1}`}
+          className={`target animation-${eatenBroccoli+ 1}`}
           onClick={eatBroccoli} // 클릭 시 먹은 브로콜리 수를 증가시키고 애니매이션 횟수 증가
         ></div>
       )}
