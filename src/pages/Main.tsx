44e83a8ea585aa80ec8fe7776b4d9d47a0e4065c
samuel21695 <samuel21695@gmail.com>
2024-02-02 12:46:53 +0900

    🎨 layouts and styling

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -6,13 +6,12 @@ function Main() {
     <div id='main'>
       <div className='css-1c'>
         <h1>Am I really hungry?</h1>
-        <p>Feel free to test that you are really hungry or just bored with the broccoli test.</p>
-        <p>Ask yourself “would I eat raw broccoli right now?”</p>
-      </div>
-      <div className='css-2c'>
         <Link to="/test">
           <div className='broccoli'></div>
         </Link>
+        <br></br>
+        <p>Feel free to test that you are really hungry or just bored with the broccoli test.</p>
+        <p>Ask yourself “would I eat raw broccoli right now?”</p>
       </div>
     </div>
   );
