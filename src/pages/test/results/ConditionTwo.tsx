19ae277bef64d04892fcf6d2c82436e21fc15190
samuel21695 <samuel21695@gmail.com>
2024-02-04 22:13:51 +0900

    🧪 test text

--- src/pages/test/results/ConditionTwo.tsx
+++ src/pages/test/results/ConditionTwo.tsx
@@ -2,7 +2,7 @@ function ConditionTwo() {
   return (
     <>
       <h1>You are now <span style={{ color: "#CAD273"}}>"Condition"</span></h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore natus soluta corrupti. Molestiae, possimus deserunt. Temporibus quod mollitia omnis quaerat ipsum a amet, fugiat et possimus illum? Assumenda, illo voluptas!</p>
+      <p>test 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore natus soluta corrupti. Molestiae, possimus deserunt. Temporibus quod mollitia omnis quaerat ipsum a amet, fugiat et possimus illum? Assumenda, illo voluptas!</p>
     </>
   );
 }
