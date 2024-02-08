bb9209679417a5d35707a84e1335438568081ae4
samuel21695 <samuel21695@gmail.com>
2024-02-08 12:43:25 +0900

    ✨ search components

--- src/pages/foods/FoodList.tsx
+++ src/pages/foods/FoodList.tsx
@@ -1,8 +1,22 @@
 import './FoodList.css'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 function FoodList () {
   return (
-    <>test</>
+    <div className='foodLists'>
+      <div className='searchGroup'>
+        <Box>
+          <TextField id="outlined-basic" label="search" color="success" variant="outlined"></TextField>
+        </Box>
+        {/* <label htmlFor="search">
+          <input type="text" id='search' />
+        </label> */}
+      </div>
+      <div className='listGroup'>
+
+      </div>
+    </div>
   )
 }
 
