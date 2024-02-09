f1ea1bff696259c56919dd930fc2adb6c2de64d1
samuel21695 <samuel21695@gmail.com>
2024-02-09 18:51:20 +0900

    ✨ create Card components

--- src/pages/foods/FoodList.tsx
+++ src/pages/foods/FoodList.tsx
@@ -2,7 +2,7 @@ import './FoodList.css'
 
 // MUI components
 import { Box, TextField } from '@mui/material';
-
+import MediaCard from '../../components/Cards';
 
 function FoodList () {
   return (
@@ -21,14 +21,14 @@ function FoodList () {
           </Box>
           <button className='searchButton'>test</button>
         </div>
-          <select className= 'filter' name="foodFilter" id="filter">
-            <option value="select"></option>
-            <option value="broccoli value">Brocolli score</option>
-            <option value="Date">Date</option>
-          </select>
+        <select className= 'filter' name="foodFilter" id="filter">
+          <option value="select"></option>
+          <option value="broccoli value">Brocolli score</option>
+          <option value="Date">Date</option>
+        </select>
       </div>
       <div className='listGroup'>
-
+        <MediaCard></MediaCard>
       </div>
     </div>
   )
