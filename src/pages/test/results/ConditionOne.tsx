6f3c0a4d4e3603fd6ca5253eb184cffa37be16bd
samuel21695 <samuel21695@gmail.com>
2024-02-12 20:28:27 +0900

    ✨ add classnames on each result page

--- src/pages/test/results/ConditionOne.tsx
+++ src/pages/test/results/ConditionOne.tsx
@@ -1,9 +1,9 @@
 function ConditionOne() {
   return (
-    <>
+    <div className="results">
       <h1>You are now in a <span style={{ color: "#2A9D20"}}>"False hunger"</span></h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore natus soluta corrupti. Molestiae, possimus deserunt. Temporibus quod mollitia omnis quaerat ipsum a amet, fugiat et possimus illum? Assumenda, illo voluptas!</p>
-    </>
+    </div>
   );
 }
 
