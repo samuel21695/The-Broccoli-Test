05d17403a702fa8533ec62bd649a4fea68142e95
samuel21695 <samuel21695@gmail.com>
2024-02-13 16:37:45 +0900

    📦 installing cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors'); 
 const app = express();
+
+app.use(cors());
 
 // Create a connection to MariaDB database
 const connection = mysql.createConnection({
