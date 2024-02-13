b23784c5bdabfcb75d9777e4fcc0f6729637ffc9
samuel21695 <samuel21695@gmail.com>
2024-02-13 12:08:52 +0900

    ✨ create connection

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
+
+// Create a connection to MariaDB database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'broccoli'
+});
