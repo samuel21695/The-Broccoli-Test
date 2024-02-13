6b67a0fb73efe6faabf691fa80c28dd084e444d5
samuel21695 <samuel21695@gmail.com>
2024-02-13 15:28:21 +0900

    ✨ function to fetch the list of foods from the server

--- src/T.tsx
+++ src/T.tsx
@@ -1,4 +1,18 @@
 function T () {
+  // Function to fetch the list of foods from the server
+  const fetchFoods = async () => {
+    try {
+      const response = await fetch('/foods'); // send a request to fetch the list of foods from the server
+      if (!response.ok) {
+        throw new Error('Failed to fetch foods'); //Throw an error if the response is not successful
+      }
+      const data = await response.json(); // Parse the response as JSON
+      setFoods(data); // Save the fetched food data to the state
+    } catch (error) {
+      console.error('Erro fetching data:', error); // Log an error message if an error occurs
+    }
+  }
+
   return (
     <>test</>
   )
