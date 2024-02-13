d1dc8e90372d5bfa815352e196c11da961e9c510
samuel21695 <samuel21695@gmail.com>
2024-02-13 15:35:24 +0900

    ✨ create state to store the list of foods
    importing usesState, useEffect from 'react', interface using typescript

--- src/T.tsx
+++ src/T.tsx
@@ -1,4 +1,16 @@
+import { useState, useEffect} from 'react';
+
+interface Food {
+  id: number;
+  name: string;
+  broccoliScore: number;
+  image: string;
+  description: string;
+}
+
 function T () {
+  // State to store the list of foods fetched from the server
+  const [foods, setFoods] = useState<Food[]>([]);
   // Function to fetch the list of foods from the server
   const fetchFoods = async () => {
     try {
@@ -9,7 +21,7 @@ function T () {
       const data = await response.json(); // Parse the response as JSON
       setFoods(data); // Save the fetched food data to the state
     } catch (error) {
-      console.error('Erro fetching data:', error); // Log an error message if an error occurs
+      console.error('Error fetching data:', error); // Log an error message if an error occurs
     }
   }
 
