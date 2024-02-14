13302822ad92e731f7a8280d5ac237c02daba85b
samuel21695 <samuel21695@gmail.com>
2024-02-14 11:30:51 +0900

    ✨Apply functions to foodList page

--- src/pages/foods/FoodList.tsx
+++ src/pages/foods/FoodList.tsx
@@ -1,10 +1,54 @@
+import { useState, useEffect} from 'react';
+import fetchFoods from '../../functions/fetchFoods';
 import './FoodList.css'
 
 // MUI components
 import { Box, TextField } from '@mui/material';
 import MediaCard from '../../components/Cards';
 
+interface Food {
+  id: number;
+  name: string;
+  broccoliScore: number;
+  image: string;
+  description: string;
+  calories:number;
+  fat:number;
+  carbs:number;
+}
+
 function FoodList () {
+    // State to store the list of foods fetched from the server
+    const [foods, setFoods] = useState<Food[]>([]);
+    // State to store the search term
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    // State to store the search result
+    const [searchResult, setSearchResult] = useState<Food[]>([]);
+  
+    useEffect(() => {
+      fetchFoods().then((data) => setFoods(data)); // Call the function to fetch the list of foods when the component mpunts
+    }, [searchTerm]);
+
+    const handleSearch = async () => {
+      try {
+        // Fetch foods from the server based on the search term
+        const response = await fetch(`http://localhost:3001/foods?search=${searchTerm}`)
+        if(!response.ok) {
+          throw new Error('Failed to search foods');
+        }
+        const data: Food[] = await response.json();
+        setSearchResult(data);
+      } catch (error) {
+        console.error('Error searching foods:', error);
+      }
+    };
+
+    const handleKeyPress = (e:React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter') {
+        // Trigger search when Enter key is pressed
+        handleSearch();
+      }
+    };
   return (
     <div className='foodLists'>
       <div className='foodSearch'>
