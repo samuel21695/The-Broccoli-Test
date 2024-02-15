770cb12a202dfef8fd3febf017b22648ef86155d
samuel21695 <samuel21695@gmail.com>
2024-02-15 12:44:44 +0900

    ✨ Change Condition one contents

--- src/pages/test/results/ConditionOne.tsx
+++ src/pages/test/results/ConditionOne.tsx
@@ -1,8 +1,13 @@
 function ConditionOne() {
   return (
     <div className="results">
-      <h1>You are now in a <span style={{ color: "#2A9D20"}}>"False hunger"</span></h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore natus soluta corrupti. Molestiae, possimus deserunt. Temporibus quod mollitia omnis quaerat ipsum a amet, fugiat et possimus illum? Assumenda, illo voluptas!</p>
+      <h1>You are now in the <span style={{ color: "#2A9D20"}}>"False hunger"</span></h1>
+      <p className="status">You are just emotionally hungry, and you are NOT actually hungry for food.
+      <br></br>
+      <br></br>
+      You might be lonely, thirsty, or just looking for a distraction. Use this opportunity to face your issues and figure out what you really need. 
+      </p>
+      <h2>Recommended foods</h2>
       <div className = "recommend foods-1">
         <div>🍎 🍌 🍑 🍒 🍓 🥬 🥦</div>
       </div>
