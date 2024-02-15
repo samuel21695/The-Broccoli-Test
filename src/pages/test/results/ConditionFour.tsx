3490eda6ea13e45037364e8749166ee7fc02ac57
samuel21695 <samuel21695@gmail.com>
2024-02-15 12:33:34 +0900

    ✨ condition content change

--- src/pages/test/results/ConditionFour.tsx
+++ src/pages/test/results/ConditionFour.tsx
@@ -3,6 +3,9 @@ function ConditionFour() {
     <div className="results">
       <h1>You are now <span style={{ color: "#FFA841"}}>"Condition"</span></h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore natus soluta corrupti. Molestiae, possimus deserunt. Temporibus quod mollitia omnis quaerat ipsum a amet, fugiat et possimus illum? Assumenda, illo voluptas!</p>
+      <div className = "recommend foods-1">
+        <div>🍎 🍌 🍑 🍒 🍓 🥬 🥦</div>
+      </div>
     </div>
   );
 }
--- src/pages/test/results/ConditionTwo.tsx
+++ src/pages/test/results/ConditionTwo.tsx
@@ -3,6 +3,9 @@ function ConditionTwo() {
     <div className="results">
       <h1>You are now <span style={{ color: "#CAD273"}}>"Condition"</span></h1>
       <p>test 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore natus soluta corrupti. Molestiae, possimus deserunt. Temporibus quod mollitia omnis quaerat ipsum a amet, fugiat et possimus illum? Assumenda, illo voluptas!</p>
+      <div className = "recommend foods-1">
+        <div>🍎 🍌 🍑 🍒 🍓 🥬 🥦</div>
+      </div>
     </div>
   );
 }
