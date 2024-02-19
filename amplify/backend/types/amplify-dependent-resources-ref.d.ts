4f4e0a14e37fb5fee1a3db99cf34f7f8f7058f47
samuel21695 <samuel21695@gmail.com>
2024-02-19 23:47:40 +0900

    📦 amplify package

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "thebroccolitest636e3952": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s34fe9f29c": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
