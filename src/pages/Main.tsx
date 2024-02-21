b20f7b8f48c321ebdc5ce83b52119938d846bf9c
samuel21695 <samuel21695@gmail.com>
2024-02-21 10:38:56 +0900

    🛠️ delete unnecessary MUI components

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import React, { Link } from 'react-router-dom'
 import './Main.css';
 
 import Test from './test/BroccoliTest';
--- src/pages/foods/FoodList.tsx
+++ src/pages/foods/FoodList.tsx
@@ -1,15 +1,6 @@
-import { useState, useEffect} from 'react';
+import React, { useState, useEffect} from 'react';
 import fetchFoods from '../../functions/fetchFoods';
 import './FoodList.css'
-
-// MUI components
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
 
 interface Food {
   id: number;
